02dd9f9dfa0528b41f459b32da1e75d3364531d1
NLevy1 <nathanaellevy@hotmail.com>
2026-03-01 10:34:11 +0100

    Add simple database functionality

--- lib/data_warehouse.dart
+++ lib/data_warehouse.dart
@@ -1,14 +1,57 @@
-import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
+import 'package:sqflite/sqflite.dart';
 
+class DatabaseOperator {
+  static Database? _database;
 
-Future<String> database_setup_and_return_version() async {
-  var factory = databaseFactoryFfiWeb;
-  var db = await factory.openDatabase ('my_db.db');
-  var sqliteVersion = (await db.rawQuery('select sqlite_version()')).first.values.first;
-  return sqliteVersion;
+  static Future<Database> getDatabase() async {
+    if (_database != null) return _database!;
+    var factory = databaseFactoryFfiWeb;
+    _database = await factory.openDatabase(
+      'my_db.db',
+      options: OpenDatabaseOptions(
+        version: 1,
+        onCreate: (db, version) async {
+          await db.execute('''
+        create table tasks (
+        id integer primary key autoincrement,
+        task_name text)
+        ''');
+        },
+      ),
+    );
+    return _database!;
+  }
+
+  static Future<String> returnDatabaseVersion() async {
+    final db = await getDatabase();
+    var sqliteVersion = (await db.rawQuery(
+      'select sqlite_version()',
+    )).first.values.first;
+    return sqliteVersion.toString();
+  }
+
+  static Future<void> insertTask(String taskName) async {
+    final db = await getDatabase();
+    await db.insert('tasks', {'task_name': taskName});
+  }
+
+  static Future<List<Map<String, dynamic>>> getTasks() async {
+    final db = await getDatabase();
+    return await db.query('tasks');
+  }
+
+  static Future<void> deleteTask(int id) async {
+    final db = await getDatabase();
+    await db.delete('tasks', where: 'id = ?', whereArgs: [id]);
+  }
 }
 
-void main() async {
-  print(await database_setup_and_return_version());
+Future<String> databaseSetupAndReturnVersion() async {
+  var factory = databaseFactoryFfiWeb;
+  var db = await factory.openDatabase('my_db.db');
+  var sqliteVersion = (await db.rawQuery(
+    'select sqlite_version()',
+  )).first.values.first;
+  return sqliteVersion.toString();
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,68 @@
 import 'package:flutter/material.dart';
+import 'data_warehouse.dart';
 
-void main() => runApp(const MaterialApp(home: Scaffold(body: Center(child: Text('Hello World')))));
+void main() => runApp(const MaterialApp(home: TaskScreen()));
+
+class TaskScreen extends StatefulWidget {
+  const TaskScreen({super.key});
+
+  @override
+  State<TaskScreen> createState() => _TaskScreenState();
+}
+
+class _TaskScreenState extends State<TaskScreen> {
+  List<Map<String, dynamic>> _tasks = [];
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshTasks();
+  }
+
+  Future<void> _refreshTasks() async {
+    final data = await DatabaseOperator.getTasks();
+    setState(() {
+      _tasks = data;
+      _isLoading = false;
+    });
+  }
+
+  Future<void> _addTask() async {
+    await DatabaseOperator.insertTask('Task #${_tasks.length + 1}');
+    _refreshTasks();
+  }
+
+  Future<void> _deleteTask(int id) async {
+    await DatabaseOperator.deleteTask(id);
+    _refreshTasks();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Advanced SQLite Web')),
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: _tasks.length,
+              itemBuilder: (context, index) {
+                final item = _tasks[index];
+                return ListTile(
+                  title: Text(item['task_name']),
+                  leading: CircleAvatar(child: Text('${item['id']}')),
+                  // Added a delete button to each row
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete, color: Colors.red),
+                    onPressed: () => _deleteTask(item['id']),
+                  ),
+                );
+              },
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addTask,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
